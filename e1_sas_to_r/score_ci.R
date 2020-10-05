5be403c2b93a3f400379e147a01951966d127cce
asewak <asewak17@gmail.com>
2020-10-05 09:54:35 +0200

    Completed score CI

--- e1_sas_to_r/score_ci.R
+++ e1_sas_to_r/score_ci.R
@@ -2,14 +2,7 @@ require(data.table)
 require(survival)
 data("GBSG2", package = "TH.data")
 
-# data <- as.data.table(GBSG2)
-# stratum <- NULL
-# t <- "time"
-# event <- "cens"
-# treatment <- "horTh" # Must only have two levels
-# treat_lvl <- "yes"
-# b <- 0
-
+# Score as a function of beta - S(b)
 fn_score <- function(dt, stratum=NULL, t, event, treatment, treat_lvl, b){
   
   # Convert to a data.table and create strata column
@@ -40,7 +33,7 @@ fn_score <- function(dt, stratum=NULL, t, event, treatment, treat_lvl, b){
   return(S_b[,sc])
 }
 
-
+# Cox PH estimate
 fn_cox <- function(dt, t, event, treatment){
   stime <- with(dt, Surv(time, cens))
   form <- as.formula(paste0("stime~",treatment))
@@ -49,23 +42,33 @@ fn_cox <- function(dt, t, event, treatment){
   return(sum_fit$coefficients[,c("coef", "se(coef)")])
 }
 
-# fn_score(dt=GBSG2, stratum=NULL, t="time", event="cens", treatment="horTh", treat_lvl="yes", b=0)
-
-est_cox <- fn_cox(dt=GBSG2, t="time", event="cens", treatment="horTh")
-
-grid_b_low <- seq(est_cox["coef"] - 2.96*est_cox["se(coef)"], est_cox["coef"], length.out = 5)
-grid_b_up <- seq(est_cox["coef"], est_cox["coef"] + 2.96*est_cox["se(coef)"], length.out = 5)
-
-
-# Interpolate to find the intersection with the relevant
+# Interpolate to find the intersection with the relevant quantile
 # Note: on the sqrt scale for speed
 fn_interpolate <- function(g, alpha, dt, stratum=NULL, t, event, treatment, treat_lvl){
   S_grid <- sapply(g, function(b) fn_score(dt=dt, stratum=stratum, t=t, event=event, treatment=treatment, treat_lvl=treat_lvl, b=b))
   s <- spline(g, sqrt(S_grid))
   int <- approx(x = s$y, y = s$x, xout = qnorm(1-alpha/2))
-  return(int)
+  return(int$y)
 }
 
-fn_interpolate(g=grid_b_low, alpha=0.05, dt=GBSG2, stratum=NULL, t="time", event="cens", treatment="horTh", treat_lvl="yes")
-fn_interpolate(g=grid_b_high, alpha=0.05, dt=GBSG2, stratum=NULL, t="time", event="cens", treatment="horTh", treat_lvl="yes")
+# Combines the score function and interpolation to output a score CI
+# Note: Assumes a 'wide' 99.7% Wald interval to determine range to search for the bounds
+fn_score_ci <- function(alpha=0.05, dt, stratum=NULL, t, event, treatment, treat_lvl){
+  
+  # Estimate the cox coefficient
+  est_cox <- fn_cox(dt=dt, t=t, event=event, treatment=treatment)
+  
+  # Create a 99% Wilcoxon CI for the range to search
+  grid_b_low <- seq(est_cox["coef"] - 2.96*est_cox["se(coef)"], est_cox["coef"], length.out = 5)
+  grid_b_up <- seq(est_cox["coef"], est_cox["coef"] + 2.96*est_cox["se(coef)"], length.out = 5)
+  
+  # Interpolate along the grid to find the intersection with relevant quantile
+  ci_low <- fn_interpolate(g=grid_b_low, alpha=alpha, dt=dt, stratum=stratum, t=t, event=event, treatment=treatment, treat_lvl=treat_lvl)
+  ci_up <- fn_interpolate(g=grid_b_up, alpha=alpha, dt=dt, stratum=stratum, t=t, event=event, treatment=treatment, treat_lvl=treat_lvl)
+  ci <- cbind(ci_low, ci_up)
+  colnames(ci) <- paste0(round(c(alpha/2,1-alpha/2) * 100,1)," %")
+  return(ci)
+}
 
+# Example usage
+fn_score_ci(alpha=0.05, dt=GBSG2, stratum=NULL, t="time", event="cens", treatment="horTh", treat_lvl="yes")
